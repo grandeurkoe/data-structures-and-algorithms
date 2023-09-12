abb8af06c768c17ee3c354d7cfed8395832aa9c5
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2023-09-12 20:30:35 +0530

    Update implementing-an-array

--- data-structures/arrays-or-lists/implementing-an-array/main.py
+++ data-structures/arrays-or-lists/implementing-an-array/main.py
@@ -1,44 +1,88 @@
 class Array:
     def __init__(self):
-        self.length = 0
         self.data = {}
+        self.length = 0
 
-    def get(self, index):
-        """Returns the element at the passed index."""
-        return self.data[index]
+    def is_empty(self):
+        """Checks if list is Empty. Returns True if Empty. """
+        if self.length == 0:
+            return self.length == 0
+        else:
+            return False
 
-    def push(self, item):
-        """Appends the item at the end of the Array. Returns the length of the Array."""
-        self.data[len(self.data)] = item
+            # DISPLAY OPERATION
+
+    # Displays the array as a string.
+    # Time complexity - O(n)
+    def display(self):
+        """Displays list as a string."""
+        if not self.is_empty():
+            array = "["
+            for each_key in self.data:
+                array += f"{self.data[each_key]},"
+            array = array[:len(array) - 1]
+            print(f"{array}]")
+        else:
+            print("[]")
+
+    # LOOKUP OPERATION
+    # To get the data element at a certain index.
+    # Since python dictionaries use hash tables, lookups are faster.
+    # Time complexity - O(1)
+    def get_data_at_index(self, index):
+        """Gets the data element at a given index. Returns the data element"""
+        if not self.is_empty():
+            return self.data[index]
+        else:
+            return "No data elements in SAMPLE."
+
+    # To get the index of a data element.
+    # Time complexity - O(n)
+    def get_index(self, element):
+        """Gets the index of a given data element. Returns the index."""
+        if not self.is_empty():
+            for each_key in self.data:
+                if self.data[each_key] == element:
+                    return each_key
+        else:
+            return "No data elements in SAMPLE."
+
+    # INSERTION OPERATION
+    # To append data element at the end of the list.
+    # Time complexity - O(1)
+    def push(self, element):
+        """Appends data element at the end of the list."""
+        self.data[f'{self.length}'] = element
         self.length += 1
-        return self.length
+        print(f'Pushed {element}.')
 
-    def delete(self):
-        """Pops and returns the last element in the Array."""
-        last_item = self.data[len(self.data) - 1]
-        del self.data[len(self.data) - 1]
-        self.length -= 1
-        return last_item
+    # DELETION OPERATION
+    # To delete data element from the end of the list.
+    # Time complexity - O(1)
+    def pop(self):
+        """Pops data element at the end of the list"""
+        if not self.is_empty():
+            del self.data[f'{self.length - 1}']
+            self.length -= 1
+        else:
+            return "No data elements in SAMPLE."
 
-    def delete_at_index(self, index):
-        """Deletes and returns the elements at the passed index."""
-        deleted_item = self.data[index]
-        self.shift_items(index)
-        self.length -= 1
-        return deleted_item
-
-    def shift_items(self, index):
-        """Shifts elements after an elements has been deleted."""
-        for shifts in range(index, len(self.data) - 1):
-            self.data[shifts] = self.data[shifts + 1]
-        del self.data[len(self.data) - 1]
+    # To delete data element at a given index.
+    # Time complexity - O(N)
+    def remove(self, index):
+        """Deletes data element at a given index."""
+        if not self.is_empty():
+            del self.data[f'{index}']
+            self.length -= 1
+        else:
+            return "No data elements in SAMPLE."
 
 
-my_array = Array()
-my_array.push('Hi')
-my_array.push('You')
-my_array.push('!')
-# my_array.delete()
-# my_array.delete()
-my_array.delete_at_index(1)
-print(my_array.data)
+SAMPLE = Array()
+SAMPLE.push('a')
+SAMPLE.push('b')
+SAMPLE.push('c')
+SAMPLE.push('d')
+SAMPLE.pop()
+SAMPLE.remove(1)
+SAMPLE.display()
