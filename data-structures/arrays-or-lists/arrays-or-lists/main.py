4be38da92df48cf9c7f16302279fc1661668f319
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2023-09-12 17:52:55 +0530

    Update arrays-or-lists

--- data-structures/arrays-or-lists/arrays-or-lists/main.py
+++ data-structures/arrays-or-lists/arrays-or-lists/main.py
@@ -1,24 +1,34 @@
-# In a 32-bit system, each character in STRINGS will use 4 shelves in the memory. So, 4 * 4 = 16 bytes of storage
-# will be used for storing STRINGS. This is a rather simplified way of looking at things.
-STRINGS = ['b', 'c', 'e', 'f', 'g']
+STRINGS = ['b', 'c', 'e', 'f']
 
-# To add an element at the end of an array.
-# Time complexity of the append() function is O(1).
+# LOOKUP OPERATION
+# For example, if I want to get the data element at a certain index. 
+# Time complexity - O(1)
+print(STRINGS[1])
 
-# To remove the last element from an array.
-# Time complexity of the pop() function for the last element is O(1).
-STRINGS.pop()
+# To get the index of a data element.
+# Time complexity - O(n)
+print(STRINGS.index('c'))
 
-# To remove a specific element from an array, we simply provide its index.
-# Time complexity of the pop() function for any arbitrary value is O(n).
-# STRINGS.pop(1)
+# INSERTION OPERATION
+# To append data element at the end of the list.
+# Time complexity - O(1)
+STRINGS.append('g')
 
-# To insert an element at a given position in an array, we use the insert function.
-# Time complexity of the insert() function is O(n).
-# Inserts an item at the beginning of the array.
+# To insert data element at a given index.
+# Time complexity - O(n)
 STRINGS.insert(0, 'a')
 
-# Inserts an item at index 3 of the array.
-STRINGS.insert(3, 'd')
+# DELETION OPERATION
+# To delete data element from the end of the list.
+# Time complexity - O(1)
+STRINGS.pop()
+
+# To delete data element at a given index.
+# Time complexity - O(n)
+del STRINGS[0]
+
+# To delete data element if you know the data you wish to delete.
+# Time complexity - O(n)
+STRINGS.remove('b')
 
 print(STRINGS)
