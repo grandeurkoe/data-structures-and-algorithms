691119054aadacfa3a41c8349a0adbb4f2af15e0
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2023-09-12 20:39:41 +0530

    Update arrays-or-lists

--- data-structures/arrays-or-lists/arrays-or-lists/main.py
+++ data-structures/arrays-or-lists/arrays-or-lists/main.py
@@ -1,34 +1,34 @@
-STRINGS = ['b', 'c', 'e', 'f']
+strings = ['b', 'c', 'e', 'f']
 
 # LOOKUP OPERATION
 # For example, if I want to get the data element at a certain index. 
 # Time complexity - O(1)
-print(STRINGS[1])
+print(strings[1])
 
 # To get the index of a data element.
 # Time complexity - O(n)
-print(STRINGS.index('c'))
+print(strings.index('c'))
 
 # INSERTION OPERATION
 # To append data element at the end of the list.
 # Time complexity - O(1)
-STRINGS.append('g')
+strings.append('g')
 
 # To insert data element at a given index.
 # Time complexity - O(n)
-STRINGS.insert(0, 'a')
+strings.insert(0, 'a')
 
 # DELETION OPERATION
 # To delete data element from the end of the list.
 # Time complexity - O(1)
-STRINGS.pop()
+strings.pop()
 
 # To delete data element at a given index.
 # Time complexity - O(n)
-del STRINGS[0]
+del strings[0]
 
 # To delete data element if you know the data you wish to delete.
 # Time complexity - O(n)
-STRINGS.remove('b')
+strings.remove('b')
 
-print(STRINGS)
+print(strings)
