5b4a40c42db78273dd285d7ae1ab9ecf8fcde0b5
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2023-09-13 17:30:11 +0530

    Update implementing-a-hash-table

--- data-structures/hash-tables-or-dictionaries/implementing-a-hash-table/main.py
+++ data-structures/hash-tables-or-dictionaries/implementing-a-hash-table/main.py
@@ -1,48 +1,74 @@
-class HashTable:
+class Dictionary:
     def __init__(self, size):
-        """Creates a hash table of given size."""
+        """Generates a hash table of fixed size."""
         self.data = [None for _ in range(size)]
         self.length = size
 
-    def hash_key(self, key):
-        """Generates a hash key. Returns a hash key."""
-        hashes = 0
+    def show_dictionary(self):
+        """Shows all entries in the hash table."""
+        print(self.data)
+
+    def is_empty(self):
+        """Checks if hash table is empty."""
+        for each_entry in self.data:
+            if each_entry is not None:
+                return False
+        return True
+
+    def generate_hash_for(self, key):
+        """Generates a hash value for a given key. Returns hash value."""
+        hash_value = 0
         for i in range(len(key)):
-            hashes = (hashes + ord(key[i]) * i) % self.length
-        return hashes
+            hash_value = (hash_value + ord(key[i]) * i) % self.length
+        return hash_value
 
-    def set(self, key, value):
-        """Generates a hash key entry based on the (key,value) provided."""
-        address = self.hash_key(key)
-        if self.data[address] is None:
-            self.data[address] = []
-
-        self.data[address] += [[key, value]]
-
-    def get(self, key):
-        """Prints the (key,value) pair based on the key provided."""
-        address = self.hash_key(key)
-        if self.data[address] is None:
-            print(f"Value for '{key}' does not exist.")
+    # LOOKUP OPERATION
+    # To get the value of a dictionary entry based on the key provided.
+    def get_entry(self, key):
+        """Gets a (key, value) entry from the hash table."""
+        hash_value = self.generate_hash_for(key)
+        if self.data[hash_value] is None:
+            print(f"{key} key doesn't exist.")
         else:
-            for item in self.data[address]:
-                if key == item[0]:
-                    print(item)
+            for each_entry in self.data[hash_value]:
+                if each_entry[0] == key:
+                    print(each_entry)
+                    return True
+            print(f"{key} key doesn't exist.")
 
-    def keys(self):
-        """Prints all the keys available in the hash table."""
-        my_keys = []
-        for element in self.data:
-            if element is not None:
-                for item in element:
-                    my_keys.append(item[0])
+            # INSERTION OPERATION
 
-        print(my_keys)
+    # To insert entries into a dictionary.
+    def insert(self, key, value):
+        """Inserts a (key, value) entry into a hash table."""
+        hash_value = self.generate_hash_for(key)
+        if self.data[hash_value] is None:
+            self.data[hash_value] = []
+        self.data[hash_value].append([key, value])
+        print(f"Pushed ['{key}', {value}].")
+
+    # DELETION OPERATION
+    # To delete entries from dictionary.
+    def remove(self, key):
+        """Removes a (key, value) entry into a hash table."""
+        hash_value = self.generate_hash_for(key)
+        if not self.is_empty():
+            if self.data[hash_value] is None:
+                print(f"'{key}' key doesn't exist.")
+            else:
+                for each_entry in range(len(self.data[hash_value])):
+                    if self.data[hash_value][each_entry][0] == key:
+                        print(f"Popped {self.data[hash_value][each_entry]}")
+                        del self.data[hash_value][each_entry]
+                        if len(self.data[hash_value]) == 0:
+                            self.data[hash_value] = None
+                        return True
+                print(f"'{key}' key doesn't exist.")
 
 
-my_hash_table = HashTable(2)
-my_hash_table.set('grapes', 10000)
-my_hash_table.set('apples', 15000)
-my_hash_table.set('oranges', 20000)
-my_hash_table.get('oranges')
-my_hash_table.keys()
+sample = Dictionary(2)
+sample.insert('grapes', 10000)
+sample.insert('apples', 15000)
+sample.insert('oranges', 20000)
+sample.remove('apples')
+sample.show_dictionary()
