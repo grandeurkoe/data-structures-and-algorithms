f5d4589de4dfe993a88c5c189942c4aa8ae59150
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2023-11-19 19:16:22 +0530

    Update main.py

--- data-structures/hash-tables-or-dictionaries/implementing-a-hash-table/main.py
+++ data-structures/hash-tables-or-dictionaries/implementing-a-hash-table/main.py
@@ -1,73 +1,99 @@
 class Dictionary:
     def __init__(self, size):
-        """Generates a hash table of fixed size."""
+        """Generate a hash table of fixed memory size."""
         self.data = [None for _ in range(size)]
         self.length = size
 
-    def show_dictionary(self):
-        """Shows all entries in the hash table."""
-        print(self.data)
-
     def is_empty(self):
-        """Checks if hash table is empty."""
-        for each_entry in self.data:
-            if each_entry is not None:
+        """Check if dictionary is empty. Return True if empty else False."""
+        for entry in self.data:
+            if entry is not None:
                 return False
         return True
 
-    def generate_hash_for(self, key):
-        """Generates a hash value for a given key. Returns hash value."""
+    def display(self):
+        """Display dictionary."""
+        if self.is_empty():
+            print("\nDictionary: {}")
+        else:
+            dict_string = "\nDictionary: {"
+            for entry in self.data:
+                if entry is None:
+                    pass
+                else:
+                    for pair in entry:
+                        dict_string += f"'{pair[0]}': {pair[1]}, "
+            dict_string = dict_string[:len(dict_string) - 2]
+            print(f"{dict_string}" + "}")
+
+    def generate_hash(self, key):
+        """Generate hash value based on key. Return hash value."""
         hash_value = 0
-        for i in range(len(key)):
-            hash_value = (hash_value + ord(key[i]) * i) % self.length
+        for char_idx in range(len(key)):
+            hash_value = (hash_value + ord(key[char_idx]) * char_idx) % self.length
         return hash_value
 
-    # LOOKUP OPERATION
-    # To get the value of a dictionary entry based on the key provided.
-    def get_entry(self, key):
-        """Gets a (key, value) entry from the hash table."""
-        hash_value = self.generate_hash_for(key)
-        if self.data[hash_value] is None:
-            print(f"{key} key doesn't exist.")
-        else:
-            for each_entry in self.data[hash_value]:
-                if each_entry[0] == key:
-                    print(each_entry)
-                    return True
-            print(f"{key} key doesn't exist.")
+    def entry_exist(self, hash_value, key):
+        """Check if dictionary entry exists. Return True if exists else return False."""
+        for entry in self.data[hash_value]:
+            if key == entry[0]:
+                return True
+        return False
 
-    # INSERTION OPERATION
-    # To insert entries into a dictionary.
+    # LOOKUP OPERATION
+    # Time complexity or BIG(O) - O(n)
+    def get_entry(self, key):
+        if self.is_empty():
+            print("No entries in dictionary.")
+        else:
+            hash_value = self.generate_hash(key)
+            if self.entry_exist(hash_value, key):
+                for entry in self.data[hash_value]:
+                    if key == entry[0]:
+                        print(f"Lookup ({entry[0]}, {entry[1]}).")
+            else:
+                print(f"Key('{key}') doesn't exist.")
+
+    # INSERT OPERATION
+    # Insert entries into a dictionary.
+    # Time complexity or BIG(O) - O(1)
     def insert(self, key, value):
-        """Inserts a (key, value) entry into a hash table."""
-        hash_value = self.generate_hash_for(key)
+        """Insert dictionary entry."""
+        hash_value = self.generate_hash(key)
         if self.data[hash_value] is None:
             self.data[hash_value] = []
+
         self.data[hash_value].append([key, value])
-        print(f"Pushed ['{key}', {value}].")
+        print(f"Insert ({key}, {value}).")
 
-    # DELETION OPERATION
-    # To delete entries from dictionary.
-    def remove(self, key):
-        """Removes a (key, value) entry into a hash table."""
-        hash_value = self.generate_hash_for(key)
-        if not self.is_empty():
-            if self.data[hash_value] is None:
-                print(f"'{key}' key doesn't exist.")
-            else:
-                for each_entry in range(len(self.data[hash_value])):
-                    if self.data[hash_value][each_entry][0] == key:
-                        print(f"Popped {self.data[hash_value][each_entry]}")
-                        del self.data[hash_value][each_entry]
-                        if len(self.data[hash_value]) == 0:
-                            self.data[hash_value] = None
-                        return True
-                print(f"'{key}' key doesn't exist.")
+    # DELETE OPERATION
+    # Remove entries from dictionary.
+    # Time complexity or BIG(O) - O(n)
+    def delete(self, key):
+        """Delete dictionary entry"""
+        if self.is_empty():
+            print("No entries in dictionary.")
+        else:
+            del_idx = 0
+            hash_value = self.generate_hash(key)
+            if self.entry_exist(hash_value, key):
+                for entry_idx in range(len(self.data[hash_value])):
+                    if key == self.data[hash_value][entry_idx][0]:
+                        del_idx = entry_idx
+                print(f"Delete ({self.data[hash_value][del_idx][0]}, {self.data[hash_value][entry_idx][1]}).")
+                self.data[hash_value].pop(del_idx)
+
+                if len(self.data[hash_value]) == 0:
+                    self.data[hash_value] = None
 
 
-sample = Dictionary(2)
-sample.insert('grapes', 10000)
-sample.insert('apples', 15000)
-sample.insert('oranges', 20000)
-sample.remove('apples')
-sample.show_dictionary()
+my_dict = Dictionary(2)
+my_dict.insert('grapes', 10000)
+my_dict.insert('apples', 15000)
+my_dict.insert('oranges', 20000)
+my_dict.get_entry('grapes')
+my_dict.delete('grapes')
+my_dict.delete('apples')
+my_dict.delete('oranges')
+my_dict.get_entry('grapes')
+my_dict.display()
