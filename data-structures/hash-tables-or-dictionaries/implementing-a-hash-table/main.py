fa6cff6dc7e2c389b8b1ffa9732bc56360dfbb1e
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2023-11-26 18:33:17 +0530

    Update main.py

--- data-structures/hash-tables-or-dictionaries/implementing-a-hash-table/main.py
+++ data-structures/hash-tables-or-dictionaries/implementing-a-hash-table/main.py
@@ -14,86 +14,75 @@ class Dictionary:
     def display(self):
         """Display dictionary."""
         if self.is_empty():
-            print("\nDictionary: {}")
+            print("\nDictionary = {}")
         else:
-            dict_string = "\nDictionary: {"
-            for entry in self.data:
-                if entry is None:
-                    pass
+            dict_display = ""
+            for mem_loc in self.data:
+                if mem_loc is not None:
+                    for entry_idx in range(len(mem_loc)):
+                        dict_display += f"'{mem_loc[entry_idx][0]}': {mem_loc[entry_idx][1]}, "
                 else:
-                    for pair in entry:
-                        dict_string += f"'{pair[0]}': {pair[1]}, "
-            dict_string = dict_string[:len(dict_string) - 2]
-            print(f"{dict_string}" + "}")
+                    pass
+                dict_display = dict_display[0: -2]
+                # dict_display += "}"
+            print("\nDictionary = " + "{" + dict_display + "}")
 
     def generate_hash(self, key):
         """Generate hash value based on key. Return hash value."""
         hash_value = 0
         for char_idx in range(len(key)):
-            hash_value = (hash_value + ord(key[char_idx]) * char_idx) % self.length
+            hash_value = (hash_value + (ord(key[char_idx]) * char_idx)) % self.length
         return hash_value
 
-    def entry_exist(self, hash_value, key):
-        """Check if dictionary entry exists. Return True if exists else return False."""
-        for entry in self.data[hash_value]:
-            if key == entry[0]:
-                return True
-        return False
-
-    # LOOKUP OPERATION
-    # Time complexity or BIG(O) - O(n)
-    def get_entry(self, key):
-        if self.is_empty():
-            print("No entries in dictionary.")
-        else:
-            hash_value = self.generate_hash(key)
-            if self.entry_exist(hash_value, key):
-                for entry in self.data[hash_value]:
-                    if key == entry[0]:
-                        print(f"Lookup ({entry[0]}, {entry[1]}).")
-            else:
-                print(f"Key('{key}') doesn't exist.")
-
-    # INSERT OPERATION
-    # Insert entries into a dictionary.
-    # Time complexity or BIG(O) - O(1)
+    # INSERT OPERATION  # -> Big O - O(1)
     def insert(self, key, value):
         """Insert dictionary entry."""
         hash_value = self.generate_hash(key)
         if self.data[hash_value] is None:
-            self.data[hash_value] = []
-
-        self.data[hash_value].append([key, value])
-        print(f"Insert ({key}, {value}).")
-
-    # DELETE OPERATION
-    # Remove entries from dictionary.
-    # Time complexity or BIG(O) - O(n)
-    def delete(self, key):
-        """Delete dictionary entry"""
-        if self.is_empty():
-            print("No entries in dictionary.")
+            self.data[hash_value] = [[key, value]]
+            print(f"Insert entry('{key}', {value}).")
         else:
-            del_idx = 0
-            hash_value = self.generate_hash(key)
-            if self.entry_exist(hash_value, key):
-                for entry_idx in range(len(self.data[hash_value])):
-                    if key == self.data[hash_value][entry_idx][0]:
-                        del_idx = entry_idx
-                print(f"Delete ({self.data[hash_value][del_idx][0]}, {self.data[hash_value][entry_idx][1]}).")
-                self.data[hash_value].pop(del_idx)
+            self.data[hash_value].append([key, value])
+            print(f"Insert entry('{key}', {value}).")
 
-                if len(self.data[hash_value]) == 0:
-                    self.data[hash_value] = None
+    # DELETE OPERATION  # -> Big O - O(n)
+    def remove(self, key):
+        """Delete dictionary entry"""
+        hash_value = self.generate_hash(key)
+        if self.is_empty():
+            print("No entries available in Dictionary.")
+        else:
+            for each_entry in self.data[hash_value]:
+                if each_entry[0] == key:
+                    print(f"Delete entry('{each_entry[0]}', {each_entry[1]})")
+                    self.data[hash_value].remove(each_entry)
+            if len(self.data[hash_value]) == 0:
+                self.data[hash_value] = None
+
+    def lookup(self, key):
+        """Check if node exists."""
+        hash_value = self.generate_hash(key)
+        if self.is_empty():
+            print("No entries available in Dictionary.")
+        else:
+            if self.data[hash_value] is not None:
+                for each_entry in self.data[hash_value]:
+                    if each_entry[0] == key:
+                        print(f"Entry('{each_entry[0]}', {each_entry[1]}) exists.")
+                        return True
+                print(f"Key('{key}') doesn't exist.")
+            else:
+                print(f"Key('{key}') doesn't exist.")
 
 
-my_dict = Dictionary(2)
-my_dict.insert('grapes', 10000)
-my_dict.insert('apples', 15000)
-my_dict.insert('oranges', 20000)
-my_dict.get_entry('grapes')
-my_dict.delete('grapes')
-my_dict.delete('apples')
-my_dict.delete('oranges')
-my_dict.get_entry('grapes')
-my_dict.display()
+dictionary = Dictionary(2)
+print("\nOPERATION LOG")
+dictionary.insert('Grapes', 10000)
+dictionary.insert('Apples', 15000)
+dictionary.insert('Oranges', 20000)
+dictionary.lookup('k')
+dictionary.lookup('Apples')
+dictionary.remove('Apples')
+dictionary.remove('Grapes')
+dictionary.remove('Oranges')
+dictionary.display()
