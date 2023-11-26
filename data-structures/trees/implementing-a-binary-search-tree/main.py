9950bc930b477e8e608e7bf87b2ebd11e33c0221
Vishal Kushwaha <133014886+grandeurkoe@users.noreply.github.com>
2023-11-26 18:41:48 +0530

    Update main.py

--- data-structures/trees/implementing-a-binary-search-tree/main.py
+++ data-structures/trees/implementing-a-binary-search-tree/main.py
@@ -1,74 +1,76 @@
 class Node:
     def __init__(self, value):
+        self.value = value
         self.left = None
         self.right = None
-        self.value = value
-
-
-def inorder_successor(current_node):
-    """Get the successor node of the node to be deleted. Return successor node."""
-    current_node = current_node.right
-    successor_node = current_node
-    while True:
-        if current_node.left is None:
-            return successor_node
-        else:
-            current_node = current_node.left
-            if successor_node.value > current_node.value:
-                successor_node = current_node
 
 
 class BinarySearchTree:
     def __init__(self):
         self.root = None
+        self.length = 0
 
-    # INSERT OPERATION
+    def inorder_successor(self, node_to_be_deleted):
+        """Get the successor node of the node to be deleted. Return successor node."""
+        traverse_node = node_to_be_deleted.right
+        successor_node = node_to_be_deleted.right
+        found_successor = False
+        while not found_successor:
+            if traverse_node.left is not None:
+                traverse_node = traverse_node.left
+                if traverse_node.value < successor_node.value:
+                    successor_node = traverse_node
+            else:
+                return successor_node
+
     def insert(self, value):
         """Add node to the binary search tree."""
         new_node = Node(value)
         if self.root is None:
             self.root = new_node
         else:
+            insert_success = False
             current_node = self.root
-            is_inserted = False
-            while not is_inserted:
-                if value > current_node.value:
-                    if current_node.right is None:
-                        current_node.right = new_node
-                        is_inserted = True
-                    else:
-                        current_node = current_node.right
-                elif value < current_node.value:
+            while not insert_success:
+                if value < current_node.value:
                     if current_node.left is None:
                         current_node.left = new_node
-                        is_inserted = True
-
+                        insert_success = True
                     else:
                         current_node = current_node.left
-        print(f"Insert node with Value({value}).")
+                elif value > current_node.value:
+                    if current_node.right is None:
+                        current_node.right = new_node
+                        insert_success = True
+                    else:
+                        current_node = current_node.right
+        self.length += 1
+        print(f"Insert Node({value}).")
 
-    # REMOVE OPERATION
     def remove(self, value):
         """Remove node from binary search tree."""
-        if self.root is None:
-            print("No node available in the Binary Search Tree.")
+        if self.length == 0:
+            print("No Nodes available in Binary Search Tree.")
         else:
             current_node = self.root
             parent_node = None
-            value_exists = True
-            while value_exists:
+            remove_failure = False
+            while not remove_failure:
                 if value == current_node.value:
-                    # CASE 1: NODE TO BE DELETED IS A LEAF NODE.
+                    # CASE 1: Delete leaf nodes.
                     if current_node.left is None and current_node.right is None:
-                        if current_node == self.root:
+                        if parent_node is None:
                             self.root = None
                         else:
                             if current_node.value < parent_node.value:
                                 parent_node.left = None
                             elif current_node.value > parent_node.value:
                                 parent_node.right = None
+                        self.length -= 1
+                        print(f"Remove Node({value}).")
+                        break
 
-                    # CASE 2: NODE TO BE DELETED IS AN INTERNAL NODE WITH 1 CHILD.
+                    # CASE 2: Delete node with a single child.
                     elif current_node.left is None or current_node.right is None:
                         if parent_node is None:
                             if current_node.left is not None:
@@ -78,62 +80,65 @@ class BinarySearchTree:
                         else:
                             if current_node.left is not None:
                                 current_node = current_node.left
+                                parent_node.left = current_node
                             elif current_node.right is not None:
                                 current_node = current_node.right
-
-                            if current_node.value < parent_node.value:
-                                parent_node.left = current_node
-                            elif current_node.value > parent_node.value:
                                 parent_node.right = current_node
+                        self.length -= 1
+                        print(f"Remove Node({value}).")
+                        break
 
-                    # CASE 3: NODE TO BE DELETED IS AN INTERNAL NODE WITH 2 CHILDREN.
+                    # CASE 3: Delete node with two children.
                     elif current_node.left is not None and current_node.right is not None:
-                        successor_node = inorder_successor(current_node)
+                        print(f"Remove Node({value}).")
+                        successor_node = self.inorder_successor(current_node)
                         self.remove(successor_node.value)
+                        print(f"Move Node({successor_node.value}) to Node({value}).")
                         current_node.value = successor_node.value
-                    print(f"Remove node with Value({value}).")
-                    value_exists = False
+                        break
 
                 elif value < current_node.value:
                     if current_node.left is None:
-                        value_exists = False
+                        print(f"Node({value}) doesn't exist.")
+                        remove_failure = True
                     else:
                         parent_node = current_node
                         current_node = current_node.left
                 elif value > current_node.value:
                     if current_node.right is None:
-                        value_exists = False
+                        print(f"Node({value}) doesn't exist.")
+                        remove_failure = True
                     else:
                         parent_node = current_node
                         current_node = current_node.right
 
-    # LOOKUP OPERATION
     def lookup(self, value):
         """Check if the node exists in the Binary Search Tree."""
-        if self.root is None:
-            print("No node available in the Binary Search Tree.")
+        if self.length == 0:
+            print("No Nodes available in Binary Search Tree.")
         else:
             current_node = self.root
-            value_exists = True
-            while value_exists:
+            found_success = False
+            while not found_success:
                 if value == current_node.value:
-                    print(f"Node[{value}] available.")
-                    value_exists = False
+                    print(f"Node({value}) exists.")
+                    found_success = True
                 elif value < current_node.value:
                     if current_node.left is not None:
                         current_node = current_node.left
                     else:
-                        print(f"Node[{value}] not available.")
-                        value_exists = False
+                        print(f"Node({value}) doesn't exist.")
+                        found_success = True
                 elif value > current_node.value:
                     if current_node.right is not None:
                         current_node = current_node.right
                     else:
-                        print(f"Node[{value}] not available.")
-                        value_exists = False
+                        print(f"Node({value}) doesn't exist.")
+                        found_success = True
 
 
 bst = BinarySearchTree()
+print("\nOPERATION LOG")
 bst.insert(9)
 bst.insert(4)
 bst.insert(6)
